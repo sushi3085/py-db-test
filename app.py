7a245fe65e272c78e26cd3fb2a7bc39100d520c5
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2022-06-25 14:52:58 +0000

    fix port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
