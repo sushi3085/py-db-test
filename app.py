5b2535aae5447cbce8f72fd3eca452cd21819d15
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2022-06-25 14:33:33 +0000

    setup done

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, abort, jsonify, render_template
+
+app = Flask(__name__)
+
+# 監聽所有來自 /try 的 Post Request
+@app.route("/try", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    # signature = request.headers['X-Line-Signature']
+    # get request body as text
+    # body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
+    # handle webhook body
+    # try:
+        # handler.handle(body, signature)
+    # except InvalidSignatureError:
+        # abort(400)
+    result = ""
+    for _ in range(8):
+        result += str(_)
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')#, port=port)
